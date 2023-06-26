fdc833041629753c23fd54de712bbc3b6bfafd8c
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-06-26 23:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
-  
+  let table  = document.getElementBy("sampleTable");
+	let tr = document.createElement("tr");
+	tr.innerHtml=`<td>New Cell1</td><td>New Cell1</td>`;
+	table.append(tr);
   
 }
